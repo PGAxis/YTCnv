97c0c7d00763496f0fda83c49d98e7a8b08d4daf
PG_Axis <axis.sixtikcz@gmail.com>
2025-08-07 12:10:20 +0200

    Update ApiDefinition.cs

--- FFmpegKitIOSBinding/ApiDefinition.cs
+++ FFmpegKitIOSBinding/ApiDefinition.cs
@@ -1,83 +1,213 @@
 using System;
 using Foundation;
 using ObjCRuntime;
-using CoreFoundation;
 
-namespace Com.Arthenica.Ffmpegkit
+namespace Com.Arthenica.FFmpegKit
 {
-    // Delegate types for callbacks used in FFmpegKit
-    // These are typedefs in the original, here we define delegates accordingly.
+    // Constants
+    [Static]
+    partial interface Constants
+    {
+        [Field("AbstractSessionDefaultTimeoutForAsynchronousMessagesInTransmit", "__Internal")]
+        int AbstractSessionDefaultTimeoutForAsynchronousMessagesInTransmit { get; }
+    }
 
-    delegate void FFmpegSessionCompleteCallback(FFmpegSession session);
-    delegate void LogCallback(IntPtr log); // You can define this more precisely if you want
-    delegate void StatisticsCallback(IntPtr stats); // Same here
-
+    // FFmpegKit Interface
     [BaseType(typeof(NSObject))]
     interface FFmpegKit
     {
-        // Sync execution with arguments
-        [Static]
-        [Export("executeWithArguments:")]
-        FFmpegSession ExecuteWithArguments(NSArray arguments);
-
-        // Async execution with arguments and complete callback
-        [Static]
-        [Export("executeWithArgumentsAsync:withCompleteCallback:")]
-        FFmpegSession ExecuteWithArgumentsAsync(NSArray arguments, FFmpegSessionCompleteCallback completeCallback);
-
-        // Async execution with arguments, complete, log and statistics callbacks
-        [Static]
-        [Export("executeWithArgumentsAsync:withCompleteCallback:withLogCallback:withStatisticsCallback:")]
-        FFmpegSession ExecuteWithArgumentsAsync(NSArray arguments, FFmpegSessionCompleteCallback completeCallback, LogCallback logCallback, StatisticsCallback statisticsCallback);
-
-        // Async execution with arguments, complete callback and dispatch queue
-        [Static]
-        [Export("executeWithArgumentsAsync:withCompleteCallback:onDispatchQueue:")]
-        FFmpegSession ExecuteWithArgumentsAsync(NSArray arguments, FFmpegSessionCompleteCallback completeCallback, DispatchQueue queue);
-
-        // Async execution with arguments, complete, log, statistics callbacks, and dispatch queue
-        [Static]
-        [Export("executeWithArgumentsAsync:withCompleteCallback:withLogCallback:withStatisticsCallback:onDispatchQueue:")]
-        FFmpegSession ExecuteWithArgumentsAsync(NSArray arguments, FFmpegSessionCompleteCallback completeCallback, LogCallback logCallback, StatisticsCallback statisticsCallback, DispatchQueue queue);
-
-        // Sync execution with command string
         [Static]
         [Export("execute:")]
         FFmpegSession Execute(string command);
 
-        // Async execution with command string and complete callback
         [Static]
         [Export("executeAsync:withCompleteCallback:")]
         FFmpegSession ExecuteAsync(string command, FFmpegSessionCompleteCallback completeCallback);
 
-        // Async execution with command string, complete, log and statistics callbacks
         [Static]
         [Export("executeAsync:withCompleteCallback:withLogCallback:withStatisticsCallback:")]
         FFmpegSession ExecuteAsync(string command, FFmpegSessionCompleteCallback completeCallback, LogCallback logCallback, StatisticsCallback statisticsCallback);
 
-        // Async execution with command string, complete callback and dispatch queue
-        [Static]
-        [Export("executeAsync:withCompleteCallback:onDispatchQueue:")]
-        FFmpegSession ExecuteAsync(string command, FFmpegSessionCompleteCallback completeCallback, DispatchQueue queue);
-
-        // Async execution with command string, complete, log, statistics callbacks, and dispatch queue
         [Static]
         [Export("executeAsync:withCompleteCallback:withLogCallback:withStatisticsCallback:onDispatchQueue:")]
-        FFmpegSession ExecuteAsync(string command, FFmpegSessionCompleteCallback completeCallback, LogCallback logCallback, StatisticsCallback statisticsCallback, DispatchQueue queue);
+        FFmpegSession ExecuteAsync(string command, FFmpegSessionCompleteCallback completeCallback, LogCallback logCallback, StatisticsCallback statisticsCallback, IntPtr queue);
 
-        // Cancel all sessions
-        [Static]
-        [Export("cancel")]
-        void Cancel();
-
-        // Cancel session by id
         [Static]
         [Export("cancel:")]
         void Cancel(long sessionId);
 
-        // List sessions
         [Static]
         [Export("listSessions")]
-        NSArray ListSessions();
+        FFmpegSession[] ListSessions();
+    }
+
+    // FFmpegSession Interface
+    [BaseType(typeof(NSObject))]
+    interface FFmpegSession
+    {
+        [Static]
+        [Export("create:")]
+        FFmpegSession Create(NSArray arguments);
+
+        [Static]
+        [Export("create:withCompleteCallback:")]
+        FFmpegSession Create(NSArray arguments, FFmpegSessionCompleteCallback completeCallback);
+
+        [Static]
+        [Export("create:withCompleteCallback:withLogCallback:withStatisticsCallback:")]
+        FFmpegSession Create(NSArray arguments, FFmpegSessionCompleteCallback completeCallback, LogCallback logCallback, StatisticsCallback statisticsCallback);
+
+        [Static]
+        [Export("create:withCompleteCallback:withLogCallback:withStatisticsCallback:withLogRedirectionStrategy:")]
+        FFmpegSession Create(NSArray arguments, FFmpegSessionCompleteCallback completeCallback, LogCallback logCallback, StatisticsCallback statisticsCallback, LogRedirectionStrategy logRedirectionStrategy);
+
+        [Export("getStatisticsCallback")]
+        StatisticsCallback GetStatisticsCallback();
+
+        [Export("getCompleteCallback")]
+        FFmpegSessionCompleteCallback GetCompleteCallback();
+
+        [Export("getAllStatisticsWithTimeout:")]
+        NSArray GetAllStatisticsWithTimeout(int waitTimeout);
+
+        [Export("getAllStatistics")]
+        NSArray GetAllStatistics();
+
+        [Export("getStatistics")]
+        NSArray GetStatistics();
+
+        [Export("getLastReceivedStatistics")]
+        Statistics GetLastReceivedStatistics();
+
+        [Export("addStatistics:")]
+        void AddStatistics(Statistics statistics);
+    }
+
+    // AbstractSession Interface
+    [BaseType(typeof(NSObject))]
+    interface AbstractSession : ISession
+    {
+        [Export("init:withLogCallback:withLogRedirectionStrategy:")]
+        IntPtr Constructor(NSArray arguments, LogCallback logCallback, LogRedirectionStrategy logRedirectionStrategy);
+
+        [Export("waitForAsynchronousMessagesInTransmit:")]
+        void WaitForAsynchronousMessagesInTransmit(int timeout);
+    }
+
+    // FFmpegSessionCompleteCallback Interface
+    [BaseType(typeof(NSObject))]
+    interface FFmpegSessionCompleteCallback
+    {
+        [Export("apply:")]
+        void Apply(FFmpegSession session);
+    }
+
+    // ReturnCode Interface
+    [BaseType(typeof(NSObject))]
+    interface ReturnCode
+    {
+        [Static]
+        [Export("isSuccess:")]
+        bool IsSuccess(ReturnCode returnCode);
+
+        [Static]
+        [Export("isCancel:")]
+        bool IsCancel(ReturnCode returnCode);
+    }
+
+    // LogCallback Interface
+    [BaseType(typeof(NSObject))]
+    interface LogCallback
+    {
+        [Export("apply:")]
+        void Apply(Log log);
+    }
+
+    // StatisticsCallback Interface
+    [BaseType(typeof(NSObject))]
+    interface StatisticsCallback
+    {
+        [Export("apply:")]
+        void Apply(Statistics statistics);
+    }
+
+    // LogRedirectionStrategy Enum
+    enum LogRedirectionStrategy
+    {
+        None,
+        Redirect,
+        RedirectWithLogFile
+    }
+
+    // Log Interface
+    [BaseType(typeof(NSObject))]
+    interface Log
+    {
+        [Export("getLevel")]
+        int Level { get; }
+
+        [Export("getMessage")]
+        string Message { get; }
+
+        [Export("getTime")]
+        double Time { get; }
+    }
+
+    // Statistics Interface
+    [BaseType(typeof(NSObject))]
+    interface Statistics
+    {
+        [Export("getVideoFrameNumber")]
+        int VideoFrameNumber { get; }
+
+        [Export("getAudioFrameNumber")]
+        int AudioFrameNumber { get; }
+
+        [Export("getVideoBitrate")]
+        int VideoBitrate { get; }
+
+        [Export("getAudioBitrate")]
+        int AudioBitrate { get; }
+
+        [Export("getStartTime")]
+        double StartTime { get; }
+
+        [Export("getEndTime")]
+        double EndTime { get; }
+
+        [Export("getDuration")]
+        double Duration { get; }
+    }
+
+    // ISession Interface
+    [Protocol]
+    interface ISession
+    {
+        [Export("getSessionId")]
+        long SessionId { get; }
+
+        [Export("getCommand")]
+        string Command { get; }
+
+        [Export("getArguments")]
+        NSArray Arguments { get; }
+
+        [Export("getState")]
+        int State { get; }
+
+        [Export("getReturnCode")]
+        ReturnCode ReturnCode { get; }
+
+        [Export("getStartTime")]
+        double StartTime { get; }
+
+        [Export("getEndTime")]
+        double EndTime { get; }
+
+        [Export("getDuration")]
+        double Duration { get; }
+
+        [Export("getFailStackTrace")]
+        string FailStackTrace { get; }
     }
 }
