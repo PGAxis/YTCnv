41d2a7062e82543842e70a76cf53d9ec8cef9dc9
PG_Axis <axis.sixtikcz@gmail.com>
2025-08-14 16:42:42 +0200

    Add files via upload

---
+++ YTCnv/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using YTCnv.Screens;
+
+namespace YTCnv
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute(nameof(Settings), typeof(Settings));
+            Routing.RegisterRoute(nameof(YouTubeSearch), typeof(YouTubeSearch));
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ YTCnv/Platforms/Android/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using AndroidX.Core.App;
+
+namespace YTCnv
+{
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    public class MainActivity : MauiAppCompatActivity
+    {
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                Window.SetStatusBarColor(Android.Graphics.Color.Transparent);
+            }
+
+            await RequestNotificationPermission();
+        }
+
+        public async Task RequestNotificationPermission()
+        {
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Tiramisu)
+            {
+                var permissionsToRequest = new List<string>();
+
+                if (CheckSelfPermission(Manifest.Permission.PostNotifications) != Permission.Granted)
+                    permissionsToRequest.Add(Manifest.Permission.PostNotifications);
+
+                if (permissionsToRequest.Count > 0)
+                {
+                    ActivityCompat.RequestPermissions(this, permissionsToRequest.ToArray(), 101);
+                }
+            }
+        }
+    }
+}
